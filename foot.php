b4a8bdfa96e941d6b5c200c79f1f4cdcbfad69f4
goyalronak <gronak93@gmail.com>
2016-10-19 20:15:43 +0530

    Add files via upload

---
+++ foot.php
@@ -0,0 +1,8 @@
+
+</div>
+<footer class="w3-container w3-red">
+  <h3>Powered By : <a href="http://www.exaltitude.com">EXALTITUDE.COM</a></h3>
+</footer>
+</div>
+</div></body>
+</html>
---
+++ head.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<head>
+<title>EXALTITUDE Send Free SMS</title>
+<meta http-equiv="expires" content="0"/>
+<meta http-equiv="Content-Language" content="en-us"/>
+<meta http-equiv="content-type" content="Text/html; charset=UTF-8"/>
+<meta name="charset" content="UTF-8"/>
+<meta name="distribution" content="Global"/>
+<meta name="rating" content="General"/>
+<meta name="robots" content="Index,follow"/>
+<meta name="revisit-after" content="3 Day"/>
+<link rel="shortcut icon" href="favicon.ico"/>
+<link rel="stylesheet" href="style.css" type="text/css"/>
+<style>
+.center-vertical {
+    position: absolute;
+    z-index: 15;
+    top: 0;
+    left: 0;
+    display: table;
+    width: 100%;
+    height: 100%;
+}
+.center-vertical .center-content {
+    display: table-cell;
+    vertical-align: middle;
+	text-align:center;
+}
+</style>
+</head>
+<body>
+<div class="center-vertical">
+<div class="center-content">
+<div class="w3-container w3-red">
+  <h1>Send Free SMS</h1>
+</div>
+<div class="w3-container w3-pale-blue w3-leftbar w3-border-blue">
